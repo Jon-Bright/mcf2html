63ec28431a237345f5b01cc75b0402676265e5d9
Jon Bright <jon@siliconcircus.com>
2020-01-06 10:50:09 +0100

    Don't proceed if XSL processing fails

--- process_cal.sh
+++ process_cal.sh
@@ -8,7 +8,10 @@ MCF=$1
 HTML=${MCF/.mcf/.html}
 echo "Processing ${MCF} to ${HTML}..."
 saxonb-xslt -s:${MCF} -xsl:${XSL_DIR}cal2html.xsl -o:${HTML}
-
+if [ $? -ne 0 ]; then
+  echo "Failed XSL processing, stopping."
+  exit
+fi
 echo "Starting Chrome with ${HTML}..."
 export T=$(mktemp -d)
 google-chrome --disable-web-security --user-data-dir=$T ${HTML}
