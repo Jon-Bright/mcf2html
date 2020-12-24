0391a5c892e2bd1969c6238ed985aa248225f637
Jon Bright <jon@siliconcircus.com>
2020-12-24 21:30:18 +0100

    Make the processing script work (in bash, still) on Windows. This works fine with the bash installed by the Windows version of git.

--- process_mcf.sh
+++ process_mcf.sh
@@ -4,17 +4,28 @@ if [ $# -ne 1 ]; then
   exit
 fi
 XSL_DIR="${0/process_mcf.sh/}"
-MCF=$1
-HTML=${MCF/.mcf/.html}
+MCF="$1"
+HTMLOUT="${MCF/.mcf/.html}"
+if [ "${MSYSTEM}" == "MINGW64" ]; then
+  # We're running on Windows (probably in the Git shell)
+  CHROME="/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+  SAXON=$(find /c/Program\ Files/Sax* -name Transform.exe)
+  HTML="file:///$(cygpath -ma ${HTMLOUT})"
+else
+  # These values work on my install of Debian Linux
+  CHROME="google-chrome"
+  SAXON="saxonb-xslt"
+  HTML="${HTMLOUT}"
+fi
 echo "Processing ${MCF} to ${HTML}..."
-saxonb-xslt -s:${MCF} -xsl:${XSL_DIR}mcf2html.xsl -o:${HTML}
+"${SAXON}" -s:${MCF} -xsl:${XSL_DIR}mcf2html.xsl -o:${HTMLOUT}
 if [ $? -ne 0 ]; then
   echo "Failed XSL processing, stopping."
   exit
 fi
 echo "Starting Chrome with ${HTML}..."
 export T=$(mktemp -d)
-google-chrome --disable-web-security --user-data-dir=$T ${HTML}
+"${CHROME}" --disable-web-security --user-data-dir=$T ${HTML}
 
 echo "Removing temporary user profile..."
 rm -rf ${T}
